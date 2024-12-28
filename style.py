55d82b688233715fe42850ad38ed553ed31aab7a
Salahuddin Rahat <salahuddinrahat995@gmail.com>
2024-12-28 17:28:00 +0600

    class

--- style.py
+++ style.py
@@ -1,1 +1,11 @@
-# my_personal_project
+class animal:
+    def __init__(self,name):
+        self.name = name
+    def report(self):
+        print("animal's name: " + self.name)
+class Dog(animal):
+    def __init__(self,name):
+        super().__init__(name)
+
+my_object = Dog("HAKKOO")
+my_object.report()
